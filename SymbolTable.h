7df53fbadd91dcd47bead4ec2052120a914c9a05
yoavst <48022265+213489628@users.noreply.github.com>
2020-01-07 21:56:06 +0200

    21:55 7/1

--- SymbolTable.h
+++ SymbolTable.h
@@ -10,20 +10,25 @@
 #include <map>
 class SymbolTable {
 	std::map<std::string, ProgVar *> *m;
+	vars = new map<std::string, double>();
  public:
 	SymbolTable() { this->m = new std::map<std::string, ProgVar *>(); }
-	~SymbolTable() { delete m; }
+	~SymbolTable() { delete m; delete vars;}
 
 	void add(std::string name, double val, std::string simulator);
 	ProgVar *get(std::string name) { return m->at(name); }
 
 	double getVal(std::string name) { return m->at(name)->GetValue(); }
 
-	void setVal(std::string name, double val) { m->at(name)->SetValue(val); }
+	void setVal(std::string name, double val) { 
+		m->at(name)->SetValue(val);
+		vars->insert(std::pair<std::string, double>(name, val));
+	}
 
 	std::string getSim(std::string name) { return m->at(name)->GetSim(); }
 
 	std::map<std::string, ProgVar *> *getMap() { return this->m; }
+	std::map<std::string, double>* getVars() { return vars;}
 };
 
 #endif //ADVANCED1_HW3__SYMBOLTABLE_H_
