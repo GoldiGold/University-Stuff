7b9d10731b08983d769fe85e872190c46dc69d25
GoldiGold <59021835+GoldiGold@users.noreply.github.com>
2020-01-09 15:54:59 +0200

    CG 9.1.2020 15:54
    only a combined main.

--- main.cpp
+++ main.cpp
@@ -31,6 +31,56 @@ int main() {
 ////  delete dvlc;
 ////  delete dvec;
 int n;
+#include <iostream>
+#include "ex1.h"
+#include "SymbolTable.h"
+#include "DefineVarRightCommand.h"
+#include "DefineVarEqCommand.h"
+#include "ServerSymbolTable.h"
+#include "Condition.h"
+#include "IfCommand.h"
+#include "SleepCommand.h"
+#include "PrintCommand.h"
+#include "PutCommand.h"
+#include "WhileCommand.h"
+#include "Parser.h"
+
+		using namespace std;
+//  auto wh = new WhileCommand("x","<=","5");
+//  auto def = new DefineVarEqCommand("x", "0");
+//  auto sleep = new SleepCommand("500");
+//  auto p1 = new PrintCommand("x");
+//  auto put = new PutCommand("x", "x+1");
+//  wh->addCommand(p1);
+//  wh->addCommand(sleep);
+//  wh->addCommand(put);
+//  def->execute("");
+//  wh->execute("");
+//  delete wh;
+		auto l = new list<string>();
+		l->push_back("var");
+		l->push_back("x");
+		l->push_back("=");
+		l->push_back("3");
+		l->push_back("while");
+		l->push_back("x");
+		l->push_back("<=");
+		l->push_back("7");
+		l->push_back("{");
+		l->push_back("Sleep");
+		l->push_back("x*500");
+		l->push_back("Print");
+		l->push_back("x");
+		l->push_back("x");
+		l->push_back("=");
+		l->push_back("x+1");
+		l->push_back("}");
+		l->push_back("Print");
+		l->push_back("\"it is done\"");
+		Parser* p = new Parser(l);
+		p->execute();
+
+		return 0;
 
 	auto command = new OpenServerCommand("5400");
 	std::cout << "sockfd: " << command->GetSockfd() << " port: " << command->GetPort() << std::endl;
@@ -44,5 +94,7 @@ int n;
 	SingletonObj::getInstance()->GetMessagesQueue()->push({"t", 1});
 	std::this_thread::sleep_for(std::chrono::milliseconds(5000));
 	std::cin >> n;
+
+
 	return 0;
 }
