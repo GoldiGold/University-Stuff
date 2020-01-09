60124c9834e07c8dc413576602aa517f282c860a
yoavst <48022265+213489628@users.noreply.github.com>
2020-01-09 13:38:13 +0200

    9/1 13:38

--- main.cpp
+++ main.cpp
@@ -10,20 +10,42 @@
 #include "PrintCommand.h"
 #include "PutCommand.h"
 #include "WhileCommand.h"
+#include "Parser.h"
 int main() {
   using namespace std;
-  auto wh = new WhileCommand("x","<=","5");
-  auto def = new DefineVarEqCommand("x", "0");
-  auto sleep = new SleepCommand("500");
-  auto p1 = new PrintCommand("x");
-  auto put = new PutCommand("x", "x+1");
-  wh->addCommand(p1);
-  wh->addCommand(sleep);
-  wh->addCommand(put);
-
-  def->execute("");
-  wh->execute("");
-//  cout<< SingletonObj::getInstance()->GetSymbolTable()->getVal("x")<<endl;
+//  auto wh = new WhileCommand("x","<=","5");
+//  auto def = new DefineVarEqCommand("x", "0");
+//  auto sleep = new SleepCommand("500");
+//  auto p1 = new PrintCommand("x");
+//  auto put = new PutCommand("x", "x+1");
+//  wh->addCommand(p1);
+//  wh->addCommand(sleep);
+//  wh->addCommand(put);
+//  def->execute("");
+//  wh->execute("");
+//  delete wh;
+  auto l = new list<string>();
+  l->push_back("var");
+  l->push_back("x");
+  l->push_back("=");
+  l->push_back("3");
+  l->push_back("while");
+  l->push_back("x");
+  l->push_back("<=");
+  l->push_back("7");
+  l->push_back("{");
+  l->push_back("Sleep");
+  l->push_back("x*500");
+  l->push_back("Print");
+  l->push_back("x");
+  l->push_back("x");
+  l->push_back("=");
+  l->push_back("x+1");
+  l->push_back("}");
+  l->push_back("Print");
+  l->push_back("\"it is done\"");
+  Parser* p = new Parser(l);
+  p->execute();
 
   return 0;
 }
