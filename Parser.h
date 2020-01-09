032db805ff087de77b35ca28bf533b81b64840ee
yoavst <48022265+213489628@users.noreply.github.com>
2020-01-09 21:14:53 +0200

    Update Parser.h

--- Parser.h
+++ Parser.h
@@ -19,10 +19,12 @@
 #include "IfCommand.h"
 #include "WhileCommand.h"
 //this class take the strings list from the lexer and create list of commands
+//the constractor create the list of the commands
+//we execute them with the func execute, and then stop the communication with the simulator
 class Parser {
-  std::list<std::string>* strList;
-  std::list<Command*>* commands;
-  std::list<std::string>::iterator it;
+  std::list<std::string>* strList;//the list from the lexer
+  std::list<Command*>* commands;//list of ready commands
+  std::list<std::string>::iterator it;//iterator over the list from the lexer
  public:
   Parser(std::list<std::string>* l);
   ~Parser() {delete strList; delete commands;}
