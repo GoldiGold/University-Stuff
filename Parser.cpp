1a84ca2e90bac8b26fed9290b8e337a3716336b0
yoavst <48022265+213489628@users.noreply.github.com>
2020-01-09 13:37:41 +0200

    9/1 13:37

--- Parser.cpp
+++ Parser.cpp
@@ -37,13 +37,65 @@ Command* Parser::createCom(){
     string var = *(++it);
     it++;
     if(!(*it).compare("->")) {
+      it++;
       string sim = *(++it);
       auto c = new DefineVarRightCommand(var, sim.substr(1, sim.length()-2));
       return c;
     } else if(!(*it).compare("<-")){
+      it++;
       string sim = *(++it);
       auto c = new DefineVarLeftCommand(var, sim.substr(1, sim.length()-2));
       return c;
+    } else if(!(*it).compare("=")){
+      string exp = *(++it);
+      auto c = new DefineVarEqCommand(var, exp);
+      return c;
+    } else {
+      throw "invalid define var command";
+    }
+  } else if(!(*it).compare("if")){
+    string exp1 = *(++it);
+    string oper = *(++it);
+    string exp2 = *(++it);
+    auto c = new IfCommand(exp1, oper, exp2);
+    it++;
+    if(!(*it).compare("{")) {
+      it++;
+      while((*it).compare("}")) {
+        Command* ic = createCom();//inner command
+        c->addCommand(ic);
+        it++;
+      }
+      return c;
+    } else {
+      throw "invalid if command";
+    }
+  } else if(!(*it).compare("while")){
+    string exp1 = *(++it);
+    string oper = *(++it);
+    string exp2 = *(++it);
+    auto c = new WhileCommand(exp1, oper, exp2);
+    it++;
+    if(!(*it).compare("{")) {
+      it++;
+      while((*it).compare("}")) {
+        Command* ic = createCom();//inner command
+        c->addCommand(ic);
+        it++;
+      }
+      return c;
+    } else {
+      throw "invalid while command";
+    }
+  } else {
+    string var = *it;
+    it++;
+    if(!(*it).compare("=")) {
+      string exp = *(++it);
+      auto c = new PutCommand(var, exp);
+      return c;
+    } else {
+      throw "invalid command";
     }
   }
 }
