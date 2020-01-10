b2f015055d66fa85e8c2964a82523bbda3260ffa
GoldiGold <59021835+GoldiGold@users.noreply.github.com>
2020-01-10 04:24:13 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,15 @@
 #include "Lexer.h"
 #include <sstream>
 
-std::vector<std::string> split2(const std::string &s, char delimiter) {
-	std::vector<std::string> tokens;
-	std::string token;
-	std::istringstream tokenStream(s);
-	while (std::getline(tokenStream, token, delimiter)) {
-		tokens.push_back(token);
-	}
-	return tokens;
-}
+// std::vector<std::string> split2(const std::string &s, char delimiter) {
+// 	std::vector<std::string> tokens;
+// 	std::string token;
+// 	std::istringstream tokenStream(s);
+// 	while (std::getline(tokenStream, token, delimiter)) {
+// 		tokens.push_back(token);
+// 	}
+// 	return tokens;
+// }
 
 int main(int argc, char **argv) {
 	using namespace std;
